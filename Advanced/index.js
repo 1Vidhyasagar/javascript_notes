b860fa6d59cfdc4ed7912c2a81017f64082245d4
Vidhyasagar <vidhyasagarmyana9598@gmail.com>
2025-03-19 14:21:18 +0530

    Event Listener concept added

---
+++ Advanced/index.js
@@ -0,0 +1,9 @@
+function click() {
+    let button = document.getElementById("button");
+    button.textContent='Hello Sagar'
+
+}
+
+let button = document.getElementById("button")
+
+button.addEventListener('click', click );
