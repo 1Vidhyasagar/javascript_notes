75f26abf8a089ab61bff0c99bee44b47546fdcf3
Vidhyasagar <vidhyasagarmyana9598@gmail.com>
2025-03-19 19:11:41 +0530

    added concept of event listener

--- Advanced/index.js
+++ Advanced/index.js
@@ -1,9 +1,20 @@
-function click() {
-    let button = document.getElementById("button");
-    button.textContent='Hello Sagar'
+// 1
+// function click() {
+//     let button = document.getElementById("button");
+//     button.textContent='Hello Sagar'
 
-}
+// }
 
-let button = document.getElementById("button")
+// let button = document.getElementById("button")
 
-button.addEventListener('click', click );
+// button.addEventListener('click', click );
+// button.removeEventListener("click", click);
+
+// 2
+// let anchorElement= document.getElementById("anchor")
+
+// anchorElement.addEventListener('click', function (event){
+//     event.preventDefault();
+//     anchorElement.textContent='Your clicked'    
+
+// })
